221957e35900beeff4390774d9644d11a8a5358e
root <abyanakhtar6@gmail.com>
2023-04-08 04:45:31 +0000

    2

--- scripts/setup_machine.sh
+++ scripts/setup_machine.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 echo "Install requires sudo."
-sudo apt install -y apt-utils 
+apt install -y apt-utils 
 cd ./src/zombie-lord
-sudo ./audio.deps
-sudo ./deb.deps
-sudo ./font.deps
-sudo ./dlchrome.sh
+./audio.deps
+./deb.deps
+./font.deps
+./dlchrome.sh
 cd ../../
 echo "Installing pulse audio scripts..."
-sudo cp -r ./src/parec-server/pulse/* /etc/pulse/
+cp -r ./src/parec-server/pulse/* /etc/pulse/
 echo "Adding browsers group..."
-sudo groupadd browsers
+groupadd browsers
 echo "(if you're not on a linux system don't worry about this step)"
 echo "You need to edit sudoers file with visudo to add"
 echo "ALL ALL=(:browsers) NOPASSWD:ALL"
 echo "Adding automatically"
-sudo grep "ALL ALL=(:browsers) NOPASSWD:ALL" /etc/sudoers || sudo tee -a /etc/sudoers > /dev/null <<EOT
+grep "ALL ALL=(:browsers) NOPASSWD:ALL" /etc/sudoers || sudo tee -a /etc/sudoers > /dev/null <<EOT
 ALL ALL=(:browsers) NOPASSWD:ALL
 EOT
 echo "Done"
